4264d905801b4185bf884355ff9b625443ae2b85
Jan Martinek <honza.martinek@gmail.com>
2020-06-19 22:14:29 +0200

    Build

--- _book/service-worker.js
+++ _book/service-worker.js
@@ -1,7 +1,7 @@
 /* global self, caches, fetch */
 /* eslint-disable no-restricted-globals */
 
-const CACHE = 'cache-33fa717';
+const CACHE = 'cache-9c05860';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
